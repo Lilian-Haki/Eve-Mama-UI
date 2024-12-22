b8ef2e8a56efeb7bbfd76d22526b50aefdb874f1
Lilian-Haki <irungulilianhaki@gmail.com>
2024-12-22 15:07:18 +0300

    second commit

--- contact-app/src/App.jsx
+++ contact-app/src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PLogin from "./components/patient/login/plogin";
 import PRegister from "./components/patient/login/pregister";
 import PDashboard from "./components/patient/dashboard/dashboard";
+import AddAppointment from "./components/patient/appointments/addApointment";
+import PendingAppointment from "./components/patient/appointments/pendingAppointment";
+import ApprovedAppointment from "./components/patient/appointments/approvedAppointments";
+import CompletedAppointments from "./components/patient/appointments/completedAppointment";
 function App() {
   return (
       <Router>
@@ -10,6 +14,10 @@ function App() {
           <Route path="login" element={<PLogin />} />
           <Route path="register" element={<PRegister/>}/>
           <Route path="dashboard" element={<PDashboard/>} />
+          <Route path="add_appointment" element={<AddAppointment/>}/>
+          <Route path="approved_appointment" element={<ApprovedAppointment/>}/>
+          <Route path="pending_appointment" element={<PendingAppointment/>}/>
+          <Route path="completed_appointment" element={<CompletedAppointments/>} />
         </Routes>
       </Router>
   );
